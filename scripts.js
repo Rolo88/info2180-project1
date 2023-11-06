935639a4dcd61882c3760dcf4572aeb1a053bd85
Rojre Byfield <rojreb@gmail.com>
2023-11-05 20:38:59 -0600

    Finished filies

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-/* Add your JavaScript to this file */
+window.addEventListener("load", () => {
+  const Form = document.querySelector("form");
+
+  Form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+
+    const displayMessage = document.querySelector(".message");
+    if (email === "") {
+      displayMessage.textContent = "Please enter a valid email address.";
+    } else {
+      displayMessage.textContent =
+        "Thank you! Your email address" +
+        " " +
+        email +
+        " " +
+        "has been added to our mailing list!";
+    }
+    document.getElementById("email").value = "";
+  });
+});
